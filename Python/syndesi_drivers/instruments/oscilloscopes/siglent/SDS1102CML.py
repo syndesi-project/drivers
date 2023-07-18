cc15645db722040d61b55fe6192fc8258b56938c
SebastienDeriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2023-07-18 18:23:56 +0200

    Add a few functions to SDDS1102CML

--- Python/syndesi_drivers/instruments/oscilloscopes/siglent/SDS1102CML.py
+++ Python/syndesi_drivers/instruments/oscilloscopes/siglent/SDS1102CML.py
@@ -5,6 +5,11 @@
 from .. import IOscilloscope
 from syndesi.adapters import IAdapter, Serial, USBVisa
 from ....protocols import SCPI
+from enum import Enum
+import re
+
+
+# ACQW : Specifi
 
 class SDS1102CML(IOscilloscope):
     def __init__(self, adapter : IAdapter) -> None:
@@ -28,3 +33,83 @@ class SDS1102CML(IOscilloscope):
 
         }
         self._prot.write(f'C{channel}:CPL {coupling_match[coupling]}')
+
+    def run_self_cal(self):
+        out = self._prot.query('*CAL?')
+        if out != '*CAL 0':
+            raise RuntimeError('Self-calibration sequence failed, response : \'{out}\'')
+
+    def clear_status_register(self):
+        self._prot.write('*CLS')
+
+    def read_error_register(self):
+        """
+        reads and clears the contents of
+        the Command error Register (CMR)
+
+        Returns
+        -------
+        code : int
+        description : str
+        """
+
+        errorStatusRegisterStructure = {
+            0 : 'No error',
+            1 : 'Unrecognized command/query header',
+            2 : 'Invalid character',
+            3 : 'Invalid separator',
+            4 : 'Missing parameter',
+            5 : 'Unrecognized keyword',
+            6 : 'String error',
+            7 : 'Parameter cannot allowed',
+            8 : 'Command String Too Long',
+            9 : 'Query cannot allowed',
+            10 : 'Missing Query mask',
+            11 : 'Invalid parameter',
+            12 : 'Parameter syntax error',
+            13 : 'Filename too long'
+        }
+        out = self._prot.query('CMR?')
+        # output is 'CMR x' with
+        pattern = r'CMR [0-9]{1,2}'
+        match = re.match(pattern, out)
+        code = int(match.group(1))
+        if code not in errorStatusRegisterStructure:
+            raise ValueError('Invalid error code in response \'{out}\'')
+        else:
+            return code, errorStatusRegisterStructure[code]
+
+
+    def save_csv(self):
+        """
+        TODO
+        """
+        pass
+
+    def get_cymometer_value(self):
+        """
+        Queries the value of the cymometer
+        When the signal frequency is less
+        than 10Hz, it returns 10Hz.
+        """
+        out = self._prot.query('CYMOMETER?')
+        # TODO : Convert 10Hz, 10kHz, 10Mhz, etc... to Hertz
+
+    def force_trigger(self):
+        """
+        Causes the instrument to make one acquisition
+        """
+        self._prot.write('FORCE_TRIGGER')
+
+    def set_filter_state(self, channel : int, state : bool):
+        """
+        Sets the filter state for the specified channel
+
+        Parameters
+        ----------
+        channel : int
+        state : bool
+        """
+        self._prot.write(f'C{channel}:FILT {"ON" if state else "OFF"}')
+        
+
